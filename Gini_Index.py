28c754d9a439e6943e52a270fb414870ae5baf5f
Ruan Pingcheng <ruanpingcheng@gmail.com>
2013-07-26 21:00:59 -0400

    Read data from Plain Text

--- Gini_Index.py
+++ Gini_Index.py
@@ -1,2 +1,8 @@
-a = 4
-print (a)
+salary_list = list();
+with open("data.txt") as file:
+    for line in file:
+        salary_list.append(int(line))
+        
+salary_list.sort()
+print (salary_list)
+        
