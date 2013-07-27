ffd2e25be259cfece3b16a172ec3bb3f5b257f7b
Ruan Pingcheng <ruanpingcheng@gmail.com>
2013-07-26 21:23:29 -0400

    Add the function to convert salary list to dot list

--- Gini_Index.py
+++ Gini_Index.py
@@ -1,8 +1,25 @@
+def sum_list(ls):
+    sum = 0
+    for element in ls:
+        sum += element
+    return sum
+
+
+def convert_salary_list_into_dots(salary_list):
+    salary_list.sort()
+    dots = list();
+    salary_sum = sum_list(salary_list)
+    sum = 0
+    for salary in salary_list:
+        sum += salary
+        dots.append(sum /salary_sum)
+    return dots;
+
+
 salary_list = list();
 with open("data.txt") as file:
     for line in file:
         salary_list.append(int(line))
         
-salary_list.sort()
-print (salary_list)
+print (convert_salary_list_into_dots(salary_list))
         
