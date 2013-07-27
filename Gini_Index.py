7bc2d805cec16e612abd5deaa590832ea83b8dd4
Ruan Pingcheng <ruanpingcheng@gmail.com>
2013-07-26 21:34:35 -0400

    Add integration and finish the whole project

--- Gini_Index.py
+++ Gini_Index.py
@@ -1,3 +1,8 @@
+def calculate_Gini_Index_from_list(salary):
+    dots = convert_salary_list_into_dots(salary)
+    integrate_result = integrate_dots_from_0_to_1(dots)
+    return 2 - 2 * integrate_result
+
 def sum_list(ls):
     sum = 0
     for element in ls:
@@ -15,11 +20,20 @@ def convert_salary_list_into_dots(salary_list):
         dots.append(sum /salary_sum)
     return dots;
 
+def integrate_dots_from_0_to_1(dots):
+    count = len(dots)
+    sum = 0
+    dots.pop()
+    for data in dots: #Pop the last item ,which is always 1
+        sum += data * 2
+    sum += 1
+    return sum / count / 2
+    
 
 salary_list = list();
 with open("data.txt") as file:
     for line in file:
         salary_list.append(int(line))
         
-print (convert_salary_list_into_dots(salary_list))
+print (calculate_Gini_Index_from_list(salary_list))
         
